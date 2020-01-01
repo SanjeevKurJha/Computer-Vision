1d2658d0ce3e1dfcb1432b2b3eb5114058f6a9c0
Sanjeev Kumar jha <46286104+SanjeevKurJha@users.noreply.github.com>
2020-01-01 21:18:34 +0530

    Add files via upload

--- Face Recognition/recognize_faces_live_final.py
+++ Face Recognition/recognize_faces_live_final.py
@@ -39,9 +39,9 @@ while True:
 
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
+    r = frame.shape[1] / float(small_frame.shape[1])
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
+    rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
     
     # Only process every other frame of video to save time
     if process_this_frame:
@@ -69,10 +69,10 @@ while True:
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        top = int(top * r)
+        right = int(right * r)
+        bottom = int(bottom * r)
+        left = int(left * r)
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
